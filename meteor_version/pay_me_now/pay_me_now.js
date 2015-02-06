81f5212fb9f55ae1dfd449f72201464584d85187
David Goldberg <DBGoldberg01@gmail.com>
2015-02-06 15:58:11 -0500

    initial commit

---
+++ meteor_version/pay_me_now/pay_me_now.js
@@ -0,0 +1,17 @@
+if (Meteor.isClient) {
+  
+
+  Template.stripeForm.helpers({
+    
+  });
+
+  Template.stripeForm.events({
+    
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
